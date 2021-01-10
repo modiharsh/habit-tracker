d9a6c945da7c7ab2bf048cd350fc00bfeb69517d
Modi, Harsh <modiharsh21@outlook.com>
2021-01-10 22:16:17 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { throwMatDialogContentAlreadyAttachedError } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'habit-tracker';
+
+  date = new FormControl(new Date());
+  today = new Date();
+  todaydate = '' + ( this.today.getMonth() +1 )+ '.' + this.today.getDate() + '.' + this.today.getFullYear();
+  selectedDate = this.todaydate;
+
+  habits = [
+    {'id':1 ,'title':'Read Books','status':1}, 
+    {'id':2 ,'title':'Workout','status':0},
+  ];
+
+  public toggleHabitButton(id){
+      
+      this.habits[id-1].status = this.habits[id-1].status ==  0 ? 1 : 0;
+      localStorage.setItem(this.selectedDate, JSON.stringify(this.habits));
+  }
+
+  public addHabit(event){
+    var newHabit = event.target.value;
+    if ( this.habits) {
+      this.habits.push({'id': this.habits.length + 1, 'title': newHabit, 'status':0 });
+    }
+    else{
+      this.habits = [ {'id': 1, 'title': newHabit, 'status':0 }]
+    }
+    localStorage.setItem(this.selectedDate, JSON.stringify(this.habits));
+    console.log(this.habits);
+  }
+
+  public dateChanged(event){
+    this.selectedDate = '' + ( event.value.getMonth() +1 )+ '.' + event.value.getDate() + '.' + event.value.getFullYear();
+    console.log(this.selectedDate);
+    var habitStatus = JSON.parse(localStorage.getItem(this.selectedDate));
+    console.log(habitStatus);
+    console.log(this.habits);
+    if (habitStatus == null){
+      this.habits.forEach(habit => {
+        habit.status = 0
+      });
+    }
+    else{
+      this.habits = habitStatus;
+    }
+    console.log(this.habits);
+  }
+
+  public readStorage(){
+    console.log(localStorage.getItem('a'));
+  }
+
+  public getTodayDate(){
+    var d = new Date();
+    
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule
+    
+   
+  ],
+  exports:[
+    MatDatepickerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
