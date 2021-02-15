724bd57656af72f3f3cb458faff6670ae066aee6
Modi, Harsh <modiharsh21@outlook.com>
2021-02-15 22:18:51 +0530

    Added habit stats component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddHabitComponent } from './add-habit/add-habit.component';
 import { ManageHabitComponent} from './manage-habit/manage-habit.component';
+import { ShowHabitStatsComponent } from './show-habit-stats/show-habit-stats.component';
 
 
 const routes: Routes = [
   { path: 'add', component: AddHabitComponent },
   { path: '', redirectTo: '/track', pathMatch: 'full' },
   { path: 'track', component: ManageHabitComponent},
+  { path: 'stats', component: ShowHabitStatsComponent},
 
 ];
 
--- src/app/show-habit-stats/show-habit-stats.component.ts
+++ src/app/show-habit-stats/show-habit-stats.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowHabitStatsComponent implements OnInit {
 
+  habits = [
+    
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadHabits();
   }
+
+  public loadHabits(){
+   
+    var habitList =  JSON.parse(localStorage.getItem('habitList'));
+    habitList.forEach(habit => {
+      var hid = "habit" + habit.id;
+      var count = JSON.parse(localStorage.getItem(hid));
+      if(!count){
+        count = 0;
+      }
+      
+      this.habits.push({'id': habit.id, 'title': habit.title , 'count': count});
+      this.habits.sort( function compare( a, b ) {
+          if ( a.count < b.count ){
+            return 1;
+          }
+          if ( a.count > b.count ){
+            return -1;
+          }
+          return 0;
+        }
+        
+      );
+      
+    });
+    
+    
+  }
+
 
 }
